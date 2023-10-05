6d6e1b7d43a726ff71ef1303573ca1f0d653d2f3
Денис <59333086+den14@users.noreply.github.com>
2023-10-05 22:16:40 +0800

    Add files via upload
    AdcRead.ino is a sketch for reading data obtained using the built-in module A0 for NodeMCU with an ESP8266 microcontroller. Also, when the magnetic field flux changes, the frequency of flashing of the LED changes.

---
+++ AdcRead.ino
@@ -0,0 +1,43 @@
+/*
+Программа опроса аналогового порта ESP8266
+Для независимой обработки цикла опроса от мигания светодиодом
+использована библиотека Ticker
+*/
+
+#include <Ticker.h>
+
+#define analogPin A0
+
+//Ticker tickerLedFlip;
+Ticker tickerAdcRead;
+
+int adcValue = 500;
+float LedDelay = 1;
+
+void LedFlip()
+{
+  int state = digitalRead(LED_BUILTIN);
+  digitalWrite(LED_BUILTIN, !state);
+}
+
+void AdcRead() {
+  adcValue = analogRead(analogPin);
+  Serial.print("ADC Value = ");
+  Serial.println(adcValue);
+  //Serial.print("LED delay = ");
+  Serial.println(int(LedDelay));
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  tickerAdcRead.attach(1, AdcRead);
+  //tickerLedFlip.attach(0.5, LedFlip);
+}
+
+void loop() {
+  LedDelay = (1.0/adcValue*550-0.6)*1000;
+  delay(int(LedDelay));
+  LedFlip();
+}
